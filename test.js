67c90065b709dea801a6a4a1c622711d61bea9c8
fujiimarine <fujiimarine@hotmail.com>
2021-09-29 22:10:21 +0900

    文字押下で音声

---
+++ test.js
@@ -0,0 +1,10 @@
+function sound(){
+    // 対象となるID名
+    var id = 'sound-file' ; 
+    // [currentTime]を認識した場合のみ、処理をする
+    if( typeof( document.getElementById( id ).currentTime ) != 'undefined' ){
+        document.getElementById( id ).currentTime = 0;
+    }
+    // [ID:sound-file]の音声ファイルを再生[play()]する
+    document.getElementById( 'sound-file' ).play() ;
+}
